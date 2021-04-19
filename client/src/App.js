791b1a8c13667b3dd5d6f8cb5bb2666814221f76
Guilherme Bernardo Del Rio <guilhermebdelrio@gmail.com>
2021-04-19 19:15:28 -0300

    Issue 6
    Implementado a busca de locais no backend, e a mostragem dos dados no front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Axios from "axios";
 
 function App() {
-
   const [nome, setNome] = useState("");
   const [descricao, setDescricao] = useState("");
   const [localizacao, setLocalizacao] = useState("");
@@ -11,70 +10,115 @@ function App() {
   const [cidade, setCidade] = useState("");
   const [uf, setUf] = useState("");
   const [tags, setTags] = useState("");
+  const [localsList, setLocalsList] = useState([]);
 
-  const submitLocal = () => {
-    Axios.post("http://localhost:3001/api/insert", {nome: nome,
-    descricao: descricao,
-    localizacao: localizacao,
-    referencia: referencia, 
-    cidade: cidade,
-    uf: uf,
-    tags: tags}).then(() => {
-      alert("Cadastrado com sucesso!");
+  const getSearch = () => {
+    Axios.get("http://localhost:3001/api/get", {
+      localsList: localsList,
+    }).then((res) => {
+      const listLocals = res.data.map((local) =>{
+        return <h6 key={local.nome}>Local: {local.nome} | Descrição: {local.descricao}</h6>
+      })
+      setLocalsList(listLocals)
     });
   };
 
+  const submitLocal = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      nome: nome,
+      descricao: descricao,
+      localizacao: localizacao,
+      referencia: referencia,
+      cidade: cidade,
+      uf: uf,
+      tags: tags,
+    }).then(() => {
+      alert("Cadastrado com sucesso!");
+    });
+  };
+  
   return (
     <div className="App">
       <div>
         <h1>MeuGuia</h1>
         <h2>testapp</h2>
       </div>
-      <div>
-        <h3>Cadastro de local</h3>
-      </div>
       <div className="form">
+        <label>Buscar </label>
+        <input
+          type="text"
+          name="busca"
+        />
+
+        <button onClick={getSearch}>Buscar</button>
+        <div>
+          <h1>{localsList}</h1>
+        </div>
+        <div>
+          <h3>Cadastro de local</h3>
+        </div>
 
         <label>Nome: </label>
-        <input type="text" name="nome" onChange={(e) => {
-          setNome(e.target.value)
-        }}
+        <input
+          type="text"
+          name="nome"
+          onChange={(e) => {
+            setNome(e.target.value);
+          }}
         />
 
         <label>Descrição: </label>
-        <input type="text" name="descricao" onChange={(e) => {
-          setDescricao(e.target.value)
-        }}
+        <input
+          type="text"
+          name="descricao"
+          onChange={(e) => {
+            setDescricao(e.target.value);
+          }}
         />
 
         <label>Localização: </label>
-        <input type="text" name="localizacao" onChange={(e) => {
-          setLocalizacao(e.target.value)
-        }}
+        <input
+          type="text"
+          name="localizacao"
+          onChange={(e) => {
+            setLocalizacao(e.target.value);
+          }}
         />
 
         <label>Ponto de referência: </label>
-        <input type="text" name="referencia" onChange={(e) => {
-          setReferencia(e.target.value)
-        }}
+        <input
+          type="text"
+          name="referencia"
+          onChange={(e) => {
+            setReferencia(e.target.value);
+          }}
         />
 
         <label>Cidade: </label>
-        <input type="text" name="cidade" onChange={(e) => {
-          setCidade(e.target.value)
-        }}
+        <input
+          type="text"
+          name="cidade"
+          onChange={(e) => {
+            setCidade(e.target.value);
+          }}
         />
 
         <label>UF: </label>
-        <input type="text" name="uf" onChange={(e) => {
-          setUf(e.target.value)
-        }}
+        <input
+          type="text"
+          name="uf"
+          onChange={(e) => {
+            setUf(e.target.value);
+          }}
         />
 
         <label>Tags (separadas por vírgula): </label>
-        <input type="text" name="tags" onChange={(e) => {
-          setTags(e.target.value)
-        }}
+        <input
+          type="text"
+          name="tags"
+          onChange={(e) => {
+            setTags(e.target.value);
+          }}
         />
 
         <button onClick={submitLocal}>Cadastrar</button>
--- server/index.js
+++ server/index.js
@@ -1,64 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "meuguiadb",
-    insecureAuth : true
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "meuguiadb",
+  insecureAuth: true,
 });
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/insert', (req, res) => {
+app.get("/api/get", (get, res) => {
+  const sqlSelect = "SELECT * FROM local";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
 
-    const nome = req.body.nome;
-    const descricao = req.body.descricao;
-    const localizacao = req.body.localizacao;
-    const referencia = req.body.referencia;
-    const cidade = req.body.cidade;
-    const uf = req.body.uf;
-    const tags = req.body.tags;
+app.post("/api/insert", (req, res) => {
+  const nome = req.body.nome;
+  const descricao = req.body.descricao;
+  const localizacao = req.body.localizacao;
+  const referencia = req.body.referencia;
+  const cidade = req.body.cidade;
+  const uf = req.body.uf;
+  const tags = req.body.tags;
 
-    var tagsArr = tags.split(','); 
+  var tagsArr = tags.split(",");
 
-    const sqlLocal = "INSERT INTO Local (nome, descricao, localizacao, referencia, Cidade_idCidade) VALUES (?, ?, ?, ?, (SELECT(idCidade) FROM Cidade WHERE nome = ? AND uf = ?));";
-    db.query(sqlLocal, [nome, descricao, localizacao, referencia, cidade, uf], (err, result) => {
-        if(!err){
-            res.json({nome, descricao, localizacao, referencia, cidade, uf})
-        }else{
-            res.status(400).json({ status: "bad request" })
-        }
-    });
-
-    const sqlTags = "INSERT INTO Tag (nome) SELECT * FROM(SELECT lower(?) AS nome) AS tmp WHERE NOT EXISTS (SELECT * FROM Tag WHERE nome = lower(?)) LIMIT 1;";
-    const sqlFks = "INSERT INTO local_has_tag (Local_idLocal, Tag_idTag) VALUES ((SELECT idLocal FROM Local WHERE nome = ?), (SELECT idTag FROM Tag WHERE nome = ?));";
-    for (var i = 0; i < tagsArr.length; i++){
-        var tag= tagsArr[i];
-        db.query(sqlTags, [tag, tag], (err, result) => {
-            if(!err){
-                res.json({tag})
-            }else{
-                res.status(400).json({ status: "bad request" })
-            }
-        });
-        db.query(sqlFks, [nome, tag], (err, result) => {
-            if(!err){
-                res.json({nome, tag})
-            }else{
-                res.status(400).json({ status: "bad request" })
-            }
-        });
+  const sqlLocal =
+    "INSERT INTO Local (nome, descricao, localizacao, referencia, Cidade_idCidade) VALUES (?, ?, ?, ?, (SELECT(idCidade) FROM Cidade WHERE nome = ? AND uf = ?));";
+  db.query(sqlLocal, [nome, descricao, localizacao, referencia, cidade, uf], (err, result) => {
+    if (!err) {
+      res.json({ nome, descricao, localizacao, referencia, cidade, uf });
+    } else {
+      res.status(400).json({ status: "bad request" });
     }
+  });
+
+  const sqlTags =
+    "INSERT INTO Tag (nome) SELECT * FROM(SELECT lower(?) AS nome) AS tmp WHERE NOT EXISTS (SELECT * FROM Tag WHERE nome = lower(?)) LIMIT 1;";
+  const sqlFks =
+    "INSERT INTO local_has_tag (Local_idLocal, Tag_idTag) VALUES ((SELECT idLocal FROM Local WHERE nome = ?), (SELECT idTag FROM Tag WHERE nome = ?));";
+  for (var i = 0; i < tagsArr.length; i++) {
+    var tag = tagsArr[i];
+    db.query(sqlTags, [tag, tag], (err, result) => {
+      if (err) {
+        //res.status(400).json({ status: "bad request" })
+      }
+    });
+    db.query(sqlFks, [nome, tag], (err, result) => {
+      if (err) {
+        //res.status(400).json({ status: "bad request" })
+      }
+    });
+  }
 });
 
 app.listen(3001, () => {
-    console.log("running on port 3001");
+  console.log("running on port 3001");
 });
